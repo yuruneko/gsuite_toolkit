ef26acdb9831b961c1bbdcadf7cbbaacd9ba146a
ken5scal <kengoscal@gmail.com>
2017-01-04 21:35:00 +0900

    Fix spacing Issue

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+
+
 package main
 
 import (
@@ -20,49 +22,40 @@ import (
 // getClient uses a Context and Config to retrieve a Token
 // then generate a Client. It returns the generated Client.
 func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	cacheFileName, err := tokenCacheFileName()
+	cacheFile, err := tokenCacheFile()
 	if err != nil {
 		log.Fatalf("Unable to get path to cached credential file. %v", err)
 	}
-	token, err := tokenFromFile(cacheFileName)
+	tok, err := tokenFromFile(cacheFile)
 	if err != nil {
-		token = getTokenFromWeb(config)
-		saveToken(cacheFileName, token)
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
 	}
-	return config.Client(ctx, token)
-}
-func saveToken(file string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to : %s\n", file)
-	f, err := os.Create(file)
-	defer f.Close()
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	json.NewEncoder(f).Encode(token)
+	return config.Client(ctx, tok)
 }
 
 // getTokenFromWeb uses Config to request a Token.
-// Ig returns  the retieved Token.
+// It returns the retrieved Token.
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
-
+	
 	var code string
 	if _, err := fmt.Scan(&code); err != nil {
 		log.Fatalf("Unable to read authorization code %v", err)
 	}
-
-	token, err := config.Exchange(context.Background(), code)
+	
+	tok, err := config.Exchange(oauth2.NoContext, code)
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web %v", err)
 	}
-	return token
+	return tok
 }
 
-// tokenCacheFiele generates credential file path/filename.
+// tokenCacheFile generates credential file path/filename.
 // It returns the generated credential path/filename.
-func tokenCacheFileName() (string, error) {
+func tokenCacheFile() (string, error) {
 	usr, err := user.Current()
 	if err != nil {
 		return "", err
@@ -74,23 +67,36 @@ func tokenCacheFileName() (string, error) {
 }
 
 // tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encounterd.
+// It returns the retrieved Token and any read error encountered.
 func tokenFromFile(file string) (*oauth2.Token, error) {
 	f, err := os.Open(file)
-	defer f.Close()
 	if err != nil {
 		return nil, err
 	}
 	t := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
 	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
 	ctx := context.Background()
+	
 	b, err := ioutil.ReadFile("client_secret.json")
 	if err != nil {
-		log.Fatalf("Unable to read client secret file : %v", err)
+		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved credentials
@@ -106,7 +112,8 @@ func main() {
 		log.Fatalf("Unable to retrieve directory Client %v", err)
 	}
 
-	r, err := srv.Users.List().Customer("my_customer").MaxResults(10).OrderBy("email").Do()
+	r, err := srv.Users.List().Customer("my_customer").MaxResults(10).
+		OrderBy("email").Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve users in domain.", err)
 	}
