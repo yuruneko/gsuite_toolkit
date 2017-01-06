4cebb5183b1a3b3a333602777b481d6b1f2c1ef3
ken5scal <kengoscal@gmail.com>
2017-01-06 21:29:04 +0900

    Hoge

---
+++ config.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+// NewClient Generate New Client
+func NewClient(scopes []string) (*http.Client, error) {
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/admin-directory_v1-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, scopes...)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	token := getToken(config)
+
+	return config.Client(context.Background(), token), err
+}
+
+func getToken(config *oauth2.Config) *oauth2.Token {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	token, err := tokenFromFile(cacheFile)
+	if err != nil {
+		token = getTokenFromWeb(config)
+		saveToken(cacheFile, token)
+	}
+	return token
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	_, err := fmt.Scan(&code)
+	if err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("admin-directory_v1-go-quickstart.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"google.golang.org/api/admin/directory/v1"
+	"fmt"
+	"log"
+	"github.com/ken5cal/gsuite_toolkit"
+)
+
+func main() {
+	scopes := []string{admin.AdminDirectoryUserReadonlyScope, admin.AdminDirectoryUserScope}
+	client, _ := NewClient(scopes)
+	srv, err := admin.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve directory Client %v", err)
+	}
+
+	// r, err := srv.Users.List().Customer("my_customer").MaxResults(10). OrderBy("email").Do()
+	r, err := srv.Users.List().Customer("my_customer").MaxResults(500).OrderBy("email").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve users in domain.", err)
+	}
+
+	if len(r.Users) == 0 {
+		fmt.Print("No users found.\n")
+	} else {
+		fmt.Print("Users:\n")
+		for _, user := range r.Users {
+			if user.PrimaryEmail == "suzuki.kengo@moneyforward.co.jp" {
+				fmt.Printf("%s (%s)\n", user.PrimaryEmail, user.Name.FullName)
+				fmt.Println(user.OrgUnitPath)
+				fmt.Println(user.Organizations)
+				user.OrgUnitPath = "/dep_ciso"
+				_, err := srv.Users.Update("suzuki.kengo@moneyforward.co.jp", user).Do()
+				if err != nil {
+					log.Fatalf("fuga", err)
+				}
+			}
+		}
+	}
+}
