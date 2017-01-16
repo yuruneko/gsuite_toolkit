3c82137a48e2374234a874cddcd591088cb3fe48
ken5scal <kengoscal@gmail.com>
2017-01-17 00:13:20 +0900

    StartImplementing Create Org Unit

--- services/structure/organizations/api.go
+++ services/structure/organizations/api.go
@@ -9,3 +9,12 @@ type Service struct {
 	*admin.OrgunitsService
 }
 
+// CreateOrganizationUnit creates an organization unit
+// POST https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits
+func (service *Service) CreateOrganizationUnit(name, parentOrgUnitPath string) (*admin.OrgUnit, error) {
+	newOrgUnit := &admin.OrgUnit{
+		Name: name,
+		ParentOrgUnitPath: parentOrgUnitPath,
+	}
+	return service.Insert("my_customer", newOrgUnit).Do()
+}
