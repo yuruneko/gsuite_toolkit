ccf7104f289eb465bf0e6e2991eafec56f6d2258
ken5scal <kengoscal@gmail.com>
2017-01-17 01:02:45 +0900

    Implemented Get Organization Units

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"log"
 
 	"github.com/ken5scal/gsuite_toolkit/client"
+	"github.com/ken5scal/gsuite_toolkit/services/structure/organizations"
 	"github.com/ken5scal/gsuite_toolkit/services/users"
 	"google.golang.org/api/admin/directory/v1"
-	"github.com/ken5scal/gsuite_toolkit/services/structure/organizations"
+
 )
 
 const (
@@ -25,7 +26,7 @@ func main() {
 	// r, err := srv.Users.List().Customer("my_customer").MaxResults(10). OrderBy("email").Do()
 	userService := &users.Service{srv.Users}
 	//r, err := userService.GetEmployees("my_customer", "email", 500)
-	user ,err := userService.GetUser("suzuki.kengo@moneyforward.co.jp")
+	user, err := userService.GetUser("suzuki.kengo@moneyforward.co.jp")
 	if err != nil {
 		log.Fatalln("Unable to retrieve users in domain.", err)
 	}
@@ -33,20 +34,18 @@ func main() {
 	fmt.Println(user)
 
 	orgUnitService := &organizations.Service{srv.Orgunits}
-	orgUnit, err := orgUnitService.CreateOrganizationUnit("セキュリティ推進グループ", "/moneyforward.co.jp/CISO室")
+	r, err := orgUnitService.GetAllOrganizationUnits()
+	//orgUnit, err := orgUnitService.CreateOrganizationUnit("セキュリティ推進グループ", "/moneyforward.co.jp/CISO室")
 	if err != nil {
 		log.Fatalln("Failed creating New Org Unit.", err)
 	}
-	fmt.Println(orgUnit)
-	//if len(r.Users) == 0 {
-	//	fmt.Print("No users found.\n")
-	//	return
-	//}
-	//
-	//for _, user := range r.Users {
-	//	if user.PrimaryEmail == "suzuki.kengo@moneyforward.co.jp" {
-	//		userService.ChangeOrgUnit(user, "dep_ciso")
-	//	}
-	//}
 
+	if len(r.OrganizationUnits) == 0 {
+		fmt.Println("No organization units found")
+		return
+	}
+
+	for _, ou := range r.OrganizationUnits {
+		fmt.Println(ou)
+	}
 }
--- services/structure/organizations/api.go
+++ services/structure/organizations/api.go
@@ -9,11 +9,17 @@ type Service struct {
 	*admin.OrgunitsService
 }
 
+// GetAllOrganizationUnits fetch all sub-organization units
+// GET https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits?orgUnitPath=full org unit path&type=all or children
+func (service *Service) GetAllOrganizationUnits() (*admin.OrgUnits, error) {
+	return service.List("my_customer").Type("all").Do()
+}
+
 // CreateOrganizationUnit creates an organization unit
 // POST https://www.googleapis.com/admin/directory/v1/customer/my_customer/orgunits
 func (service *Service) CreateOrganizationUnit(name, parentOrgUnitPath string) (*admin.OrgUnit, error) {
 	newOrgUnit := &admin.OrgUnit{
-		Name: name,
+		Name:              name,
 		ParentOrgUnitPath: parentOrgUnitPath,
 	}
 	return service.Insert("my_customer", newOrgUnit).Do()
