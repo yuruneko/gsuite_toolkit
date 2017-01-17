caff892bdeb5bfc19317a2a098053df6f7e429af
ken5scal <kengoscal@gmail.com>
2017-01-17 14:55:48 +0900

    Add More

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/api/admin/directory/v1"
 
 	"fmt"
+	"github.com/ken5scal/gsuite_toolkit/services/structure/organizations"
 )
 
 const (
@@ -38,11 +39,25 @@ func main() {
 		log.Fatalln("Failed Changing user's Organizaion unit.", err)
 	}
 
-	//orgUnitService := &organizations.Service{srv.Orgunits}
-	//r, err := orgUnitService.CreateOrganizationUnit("セキュリティ推進グループ", "/dept_ciso")
-	//if err != nil {
-	//	log.Fatalln("Failed creating New Org Unit.", err)
-	//}
+	parentName := "CISO室"
+	units := []string{"サービスインフラグループ", "社内インフラグループ", "情報セキュリティ管理部"}
+	orgUnitService := &organizations.Service{srv.Orgunits}
+
+	if len(units) < 1 {
+		return
+	}
+
+	_, err = orgUnitService.GetOrganizationUnit(parentName)
+	if err != nil {
+		log.Fatalf("Could Not find Path: %v, %v", parentName, err)
+	}
+
+	for _, unitName := range units {
+		_, err = orgUnitService.CreateOrganizationUnit(unitName, "/" + parentName)
+		if err != nil {
+			log.Fatalln("Failed creating New Org Unit.", err)
+		}
+	}
 	//
 	//fmt.Println(r)
 	//r, err := orgUnitService.GetOrganizationUnit("dept_ciso/セキュリティ推進グループ")
