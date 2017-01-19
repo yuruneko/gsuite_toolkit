748a01936772dc5f5180975888c100baf14f6e9e
ken5scal <kengoscal@gmail.com>
2017-01-19 19:06:52 +0900

    Add ugly line that checks single account 2fs status

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/ken5scal/gsuite_toolkit/client"
 	"github.com/ken5scal/gsuite_toolkit/services/users"
-	"google.golang.org/api/admin/directory/v1"
+
 	"encoding/csv"
 	"os"
 	"io"
 	"strings"
-	"net/http"
-	"io/ioutil"
 	"fmt"
+	admin "google.golang.org/api/admin/directory/v1"
+	report  "google.golang.org/api/admin/reports/v1"
+
 )
 
 const (
@@ -20,7 +21,7 @@ const (
 )
 
 func main() {
-	scopes := []string{admin.AdminDirectoryOrgunitScope, admin.AdminDirectoryUserScope}
+	scopes := []string{admin.AdminDirectoryOrgunitScope, admin.AdminDirectoryUserScope, report.AdminReportsAuditReadonlyScope, report.AdminReportsUsageReadonlyScope}
 	c := client.NewClient(clientSecretFileName, scopes)
 	srv, err := admin.New(c.Client)
 	if err != nil {
@@ -28,36 +29,68 @@ func main() {
 	}
 
 	userService := &users.Service{srv.Users}
-	//r, err := userService.GetEmployees("my_customer", "email", 500)
-
+	//_, err := userService.GetEmployees("my_customer", "email", 500)
+	//_, err = userService.ChangeOrgUnit(user, "社員・委託社員・派遣社員・アルバイト")
 	user, err := userService.GetUser("suzuki.kengo@moneyforward.co.jp")
-	_, err = userService.ChangeOrgUnit(user, "社員・委託社員・派遣社員・アルバイト")
 	if err != nil {
 		log.Fatalln("Failed Changing user's Organizaion unit.", err)
 	}
+	fmt.Println(user)
+
+	reportService, err := report.New(c.Client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve reports Client %v", err)
+	}
+
+	r, err := reportService.
+	UserUsageReport.
+		Get("suzuki.kengo@Moneyforward.co.jp", "2017-01-17").
+		Parameters("accounts:is_2sv_enrolled,accounts:is_2sv_enforced").
+		Do()
+	//r, err := reportService.Activities.List("all", "login").MaxResults(10).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve logins to domain.", err)
+	}
+
+	for _, param := range r.UsageReports[0].Parameters {
+		fmt.Printf("%v: %v\n", param.Name , param.BoolValue)
+	}
+
+	//if len(r.Items) == 0 {
+	//	fmt.Println("No logins found.")
+	//} else {
+	//	fmt.Println("Logins:")
+	//	for _, a := range r.Items {
+	//		t, err := time.Parse(time.RFC3339Nano, a.Id.Time)
+	//		if err != nil {
+	//			fmt.Println("Unable to parse login time.")
+	//			// Set time to zero.
+	//			t = time.Time{}
+	//		}
+	//		fmt.Printf("%s: %s %s\n", t.Format(time.RFC822), a.Actor.Email,
+	//			a.Events[0].Name)
+	//	}
+	//}
 
 	//orgUnitService := &organizations.Service{srv.Orgunits}
 	//_, err = orgUnitService.CreateOrganizationUnits("CISO室", []string{"セキュリティ推進グループ", "サービスインフラグループ", "社内インフラグループ", "情報セキュリティ管理部"})
 	//_, err = orgUnitService.GetOrganizationUnit("CISO室/セキュリティ推進グループ")
 	//_, err = orgUnitService.UpdateOrganizationUnit(r, "CISO室")
 
-	payload := constructPayload("/Users/suzuki/Desktop/org_structure.csv")
-	fmt.Println(payload)
-	url := "https://www.googleapis.com/batch"
+	//payload := constructPayload("/Users/suzuki/Desktop/org_structure.csv")
+	//fmt.Println(payload)
+	//url := "https://www.googleapis.com/batch"
 	//
-	//payload := strings.NewReader("--batch_0123456789\nContent-Type: application/http\nContent-ID: <item1:suzuki.kengo@moneyforward.co.jp>\n\nGET https://www.googleapis.com/admin/directory/v1/users/suzuki.kengo@moneyforward.co.jp\n\n--batch_0123456789\nContent-Type: application/http\nContent-ID: <item2:suzuki.kengo@moneyforward.co.jp>\n\nGET https://www.googleapis.com/admin/directory/v1/users/ichikawa.takashi@moneyforward.co.jp\n\n--batch_0123456789--")
+	//req, _ := http.NewRequest("POST", url, strings.NewReader(payload))
+	//req.Header.Add("content-type", "multipart/mixed; boundary=batch_0123456789")
+	//req.Header.Add("authorization", "Bearer someToken")
+	//res, _ := c.Do(req)
 	//
-	req, _ := http.NewRequest("POST", url, strings.NewReader(payload))
-	//
-	req.Header.Add("content-type", "multipart/mixed; boundary=batch_0123456789")
-	req.Header.Add("authorization", "Bearer someToken")
-	res, _ := c.Do(req)
-	//
-	defer res.Body.Close()
-	_, err = ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//defer res.Body.Close()
+	//_, err = ioutil.ReadAll(res.Body)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
 }
 
 func constructPayload(filePath string) string {
@@ -88,9 +121,10 @@ func constructPayload(filePath string) string {
 
 func RequestLine(method string, email string) string {
 	//return "GET https://www.googleapis.com/admin/directory/v1/users/" +  email
-	return method + "https://www.googleapis.com/admin/directory/v1/users/" +  email
+	return method + " " + "https://www.googleapis.com/admin/directory/v1/users/" +  email + "\n" +
+		"Content-Type: application/json\n\n" + Body()
 }
 
 func Body() string {
-
+	return "{\n" + "\"orgUnitPath\": \"/社員・委託社員・派遣社員・アルバイト\"\n" + "}\n"
 }
